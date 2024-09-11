09c07df0b1dbde90136970fecacd045df4880e52
NotZeabel <111667889+NotZeabel@users.noreply.github.com>
2024-09-10 23:47:45 -0600

    User Model

---
+++ ApiWeb/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using StackExchange.Redis;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.IdentityModel.Tokens;
+using System.Runtime.Serialization;
+
+
+namespace ApiWeb.Models
+{
+    public class User
+    {
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Id { get; set; }
+
+        public User(string email, string password, string name, string id)
+        {
+            Email = email;
+            Password = password;
+            Name = name;
+            Id = id;
+        }
+    }
+}
