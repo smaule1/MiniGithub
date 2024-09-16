fb6d084db9bbf1c837a93a142367a512a6c9a070
smaule1 <143682799+smaule1@users.noreply.github.com>
2024-09-16 14:02:57 -0600

    tabla con repositorios simple

--- WebApp/wwwroot/js/pages/Repositories.js
+++ WebApp/wwwroot/js/pages/Repositories.js
@@ -1,4 +1,6 @@
-﻿console.log("it woooooooorks");
+﻿
+
+console.log("it woooooooorks");
 
 
 getData();
@@ -11,10 +13,30 @@ async function getData() {
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
-
-        const json = await response.json();
-        console.log(json);
+        
+        const json = await response.json();                        
+        
+        for (let i of json) {
+            displayRepository(i);  
+            console.log(i);
+        }
+        
     } catch (error) {
         console.error(error.message);
     }
+}
+
+
+function displayRepository(value) {
+    const table = $("#tableBodyRepo");
+    var row = `
+        <tr>
+            <th>${value.name}</th>
+            <td>${value.visibility} </td>
+            <td>${value.tags}</td>
+        </tr>
+    `;
+    table.append(row);
+
+    table.appendChild
 }
