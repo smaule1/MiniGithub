a56229d39b0c4b4c50d27ac94ba2c16096935672
Adiraell <arayacastilloj@gmail.com>
2024-09-20 19:02:28 -0600

    algunas funciones en servicios

---
+++ ApiWeb/Controllers/RecommendationController.cs
@@ -0,0 +1,33 @@
+﻿using ApiWeb.Services;
+using Azure.Identity;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Core.Types;
+using System.Data;
+using System.Net;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecommendationController : ControllerBase
+    {
+        private readonly RecommendationService recommendationDB;
+
+        public RecommendationController(RecommendationService recommendationDB)
+        {
+            this.recommendationDB = recommendationDB;
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public async Task<IActionResult> Create(string id)
+        {
+             recommendationDB.AddPerson(id);
+
+            return Ok();
+        }
+
+    }
+}
